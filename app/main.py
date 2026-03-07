bbf1e0ecb2ddb970ea6e2ab36f33c99f2f654d5d
Rishik <rishikreddy.yesgari@gmail.com>
2026-03-07 13:07:27 -0500

    Add dockercompose

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173")
 
-@app.route("/health")
+
+@app.route("/")
 def health():
-    return jsonify({
-        "status": "ok",
-        "message": "Backend is working"
-    })
+    return jsonify({"status": "ok", "message": "Backend is working"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
