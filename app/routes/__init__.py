c8715ea8a7d782342b3b22e200d4074f3ee82d3e
ksoliman05 <kerloussoliman05@gmail.com>
2026-03-15 19:00:04 -0400

    Implemented Deleting an Account

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from .test import test_bp
-
+from .client import client_bp
 
 def register_routes(app):
     app.register_blueprint(test_bp, url_prefix="/test")
+    app.register_blueprint(client_bp, url_prefix="/client")
---
+++ app/services/client/accountService.py
@@ -0,0 +1,10 @@
+# app/services/client/account_service.py
+from app.services import run_query 
+
+def delete_account_service(user_id):
+    query = "DELETE FROM users_immutables WHERE user_id = :id"
+    params = {"id": user_id}
+    
+    # We set commit=True because we are changing the database 
+    return run_query(query, params=params, fetch=False, commit=True) 
+
