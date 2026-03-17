9d08692134eed7314fb84c99fa44f713bedaa450
ksoliman05 <kerloussoliman05@gmail.com>
2026-03-17 15:22:41 -0400

    Fix Delete to use State instead of User ID via URL

--- app/routes/client/delete.py
+++ app/routes/client/delete.py
@@ -1,13 +1,16 @@
 # app/routes/client/delete.py
 from . import client_bp 
 from app.services.client.accountService import delete_account_service
-from flask import jsonify
+from flask import jsonify, session
 
-@client_bp.route("/delete/<int:user_id>", methods=["DELETE"])
-def delete_user_account(user_id):
+@client_bp.route("/delete", methods=["DELETE"])
+def delete_user_account():
+    u_id = session.get('user_id')
+
     try:
         # We call the logic from the service layer 
-        result = delete_account_service(user_id=user_id)
-        return jsonify({"message": "Success"}), 200 
+        delete_account_service(user_id=u_id)
+        session.clear()
+        return jsonify({"message": "Account deleted successfully"}), 200 
     except Exception as e:
-        return {"error": str(e)}, 500 
+        return jsonify({"error": str(e)}), 500 
