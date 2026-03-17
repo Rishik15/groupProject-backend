5b5ae557daac5f18cbf8a9391681c6e3228ed41e
grugg1233 <gma35@njit.edu>
2026-03-17 15:48:02 -0400

    using update instead of insert because at the creation of a new user the FK dependency creates a new entry in coach and user_mutables automatically

--- app/services/onboarding/onboardUser.py
+++ app/services/onboarding/onboardUser.py
@@ -2,12 +2,16 @@ from app.services import run_query
 
 def onboardClientSurvey(user_id: int, profile_picture: str, weight: float, height: float, goal_weight: float):
 
-    try:
-
+    try: 
         run_query(
             """
-            INSERT INTO users_mutables (user_id, profile_picture, weight, height, goal_weight)
-            VALUES (:user_id, :profile_picture, :weight, :height, :goal_weight)
+            UPDATE user_mutables 
+            SET  
+                profile_picture = :profile_picture,
+                weight = :weight,
+                height = :height,
+                goal_weight = :goal_weight
+            WHERE user_id = :user_id; 
         """,
             {"user_id": user_id, "profile_picture": profile_picture, "weight": weight, "height": height, "goal_weight": goal_weight },
             fetch=False,
@@ -23,8 +27,12 @@ def onboardCoachSurvey(user_id: int, desc: str, price : float ):
 
         run_query(
             """
-            INSERT INTO coach (user_id, coach_description, price)
-            VALUES (:user_id, :desc, :price)
+            UPDATE coach
+            SET  
+                coach_description = :desc
+                price = :price
+            WHERE 
+                user_id = :user_id; 
         """,
             {"user_id": user_id, "desc": desc, "price": price},
             fetch=False,
