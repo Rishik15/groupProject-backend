e7a7bdd7bd57ecfb4c7efdd3905f8844edfd6a72
ksoliman05 <kerloussoliman05@gmail.com>
2026-03-19 15:23:56 -0400

    Implemented coach being able to update their profile (Description and Price)

---
+++ app/routes/coach/updateCoach.py
@@ -0,0 +1,17 @@
+from flask import jsonify, request, session
+
+from backend.app.services.coach.update_coach import update_coach_profile
+from . import coach_bp
+
+@coach_bp.route("/update", methods=["PATCH"])
+def handle_update():
+    try:
+        user_id = session.get("user_id")
+        data = request.get_json()
+
+        result = update_coach_profile(user_id, data)
+
+        return jsonify(result), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
---
+++ app/services/coach/update_coach.py
@@ -0,0 +1,32 @@
+from backend.app.services import run_query
+
+
+def update_coach_profile(coach_id, update_json):
+    query = "UPDATE coach SET "
+    params = {"id": coach_id}
+    
+    has_fields = False
+
+    if "price" in update_json:
+        query += "price = :price"
+        params["price"] = update_json["price"]
+        has_fields = True
+
+    if "coach_description" in update_json:
+        if has_fields:
+            query += ", "
+        
+        query += "coach_description = :desc"
+        params["desc"] = update_json["coach_description"]
+        has_fields = True
+
+    if not has_fields:
+        return {"message": "No valid fields provided"}, 400
+
+    query += " WHERE coach_id = :id"
+
+    try:
+        run_query(query, params=params, fetch=False, commit=True)
+        return {"message": "Profile Updated successfully"}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
