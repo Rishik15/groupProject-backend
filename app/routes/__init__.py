b72f59a5dfeb6dfeff3938166625d2a24e75088f
grugg1233 <gma35@njit.edu>
2026-03-19 15:59:58 -0400

    Success and failure messages, op Coaches route  moved into 'landing'

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
 from .test import test_bp
 from .auth import auth_bp
 from .client import client_bp
-from .topCoaches import topCoaches_bp
+from .landing import topCoaches_bp
 def register_routes(app):
     app.register_blueprint(test_bp, url_prefix="/test")
     app.register_blueprint(auth_bp, url_prefix="/auth")
--- app/routes/topCoaches/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-topCoaches_bp = Blueprint("topCoaches", __name__)
-
-from . import topFiveCoaches
