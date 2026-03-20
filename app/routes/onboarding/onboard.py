98d9163f9b0056a7a48c4aa15205a830302da285
grugg1233 <gma35@njit.edu>
2026-03-19 20:23:56 -0400

    After coorespondance with THE  Kevin Lee Drake , I have completed an update to the onboarding feature branch that will allow for coaches and users to have a onboarding experience more in line with that which we hath presented at runtime in the course. Rishik I kindly request that you review the presented code and ensure functionality and acceptability into the code base. Thank you for your time

--- app/routes/onboarding/onboard.py
+++ app/routes/onboarding/onboard.py
@@ -1,52 +1,113 @@
 from . import onboard_bp
 # from app.services.
 from flask import jsonify, session, request
-
+from datetime import datetime
 from app.services.onboarding import onboardUser
 
 @onboard_bp.route("/", methods=["POST"])
 def onboardSurvey():
+    try:    
+        data = request.get_json()
 
-    data = request.get_json()
+        u_id = session.get("user_id")
+        role = session.get("role")
 
-    u_id = session.get("user_id")
-    role = session.get("role")
-
-    if not u_id or not role:
-        return jsonify({"error": "Unauthorized"}), 401
-    u_id = int(u_id)
+        if not u_id or not role:
+            return jsonify({"error": "Unauthorized"}), 401
+        u_id = int(u_id)
 
 
-    if role == 'client':
-        try:
-            onboardUser.onboardClientSurvey(
-                u_id, 
-                data["profile_picture"], 
-                data["weight"],
-                data["height"],
-                data["goal_weight"]
+        date = data.get("dob").fromisoformat() if data.get("dob") is not None else datetime.now() 
+        gw = data.get("goal_weight") if data.get("goal_weight") is not None else data.get("weight")
+        pfp = data.get("profile_picture"), 
+        weight = data.get("weight"),
+        height = data.get("height"),
+
+
+        required_fields = [date, weight, height]
+        if any(field is None for field in required_fields):
+            return jsonify({"error": "weight, height, date of birth are required field"}), 400
+
+        if role == 'client':
+            try:
+                #In the future we may optionally accept the users skill (this will require a schema update)
+                onboardUser.onboardClientSurvey(
+                    u_id, 
+                    pfp, 
+                    weight, 
+                    height,
+                    gw,
+                    date
+                    )
+                return jsonify({"message": "Onboarding completed successfully"}), 200
+            except Exception as e: 
+                raise e
+            
+        elif role == 'coach':
+            desc = data.get("coach_description")
+            price = data.get("price")
+            required_fields_coach = [desc, price]
+            if any(field is None for field in required_fields_coach):
+                return jsonify({"error": "weight, height, date of birth are required field"}), 400
+            try:
+                onboardUser.onboardClientSurvey(
+                    u_id, 
+                    pfp, 
+                    weight, 
+                    height,
+                    gw,
+                    date
                 )
-            return jsonify({"message": "Onboarding completed successfully"}), 200
-        except Exception as e: 
-            raise e
-    elif role == 'coach':
-        try:
-            onboardUser.onboardClientSurvey(
-                u_id, 
-                data["profile_picture"],
-                data["weight"],
-                data["height"],
-                data["goal_weight"]
-            )
 
-            onboardUser.onboardCoachSurvey(
-                u_id, 
-                data["coach_description"],
-                data["price"]
-            )
-            return jsonify({"message": "Onboarding completed successfully"}), 200
-        except Exception as e: 
-            raise e
- 
-    else: 
-        return jsonify({"error": "Invalid role"}), 400
+                onboardUser.onboardCoachSurvey(
+                    u_id, 
+                    desc, 
+                    price
+                )
+                
+                
+                n_c = data.get("num_cert") if data.get("num_cert") is not None else 0  
+                n_c = int(n_c)
+                i = 0
+                for i in range(0, n_c): 
+                    coach_id = u_id, 
+                    cert_name = data.get("cert_name"),
+                    provider_name = data.get("provider_name"),
+                    description = data.get("description"),
+                    issued_date = data.get("issued_date").fromisoformat(), 
+                    expires_date = data.get("expires_date").fromisoformat()
+                    onboardUser.insertCoachCert(
+                        coach_id,
+                        cert_name,
+                        provider_name, 
+                        description,
+                        issued_date,
+                        expires_date,
+                    )
+                    i+=1
+                
+                n_d = data.get("num_days") if data.get("num_days") is not None else 0
+                i = 0
+                coach_id = u_id 
+                dow = data.get("day_of_week")
+                st = data.get("start_time")
+                et = data.get("end_time")
+                rec = data.get("recurring")
+                active = data.get("active")
+                for i in range (0, int(n_d)):
+                    onboardUser.coachAvailability(
+                        coach_id,
+                        dow,
+                        st,
+                        et, 
+                        rec, 
+                        active
+                    ) 
+                return jsonify({"message": "Onboarding completed successfully"}), 200
+            except Exception as e: 
+                raise e
+    
+        else: 
+            return jsonify({"error": "Invalid role"}), 400
+    except Exception as e: 
+        raise e 
--- app/services/onboarding/onboardUser.py
+++ app/services/onboarding/onboardUser.py
@@ -1,7 +1,7 @@
 from app.services import run_query
-
-def onboardClientSurvey(user_id: int, profile_picture: str, weight: float, height: float, goal_weight: float):
-
+from datetime import datetime
+def onboardClientSurvey(user_id: int,  profile_picture: str, weight: float, height: float, goal_weight: float, dob = datetime.now()):
+    #In the future we may optionally accept the users skill (this will require a schema update)
     try: 
         run_query(
             """
@@ -12,12 +12,23 @@ def onboardClientSurvey(user_id: int, profile_picture: str, weight: float, heigh
                 height = :height,
                 goal_weight = :goal_weight
             WHERE user_id = :user_id; 
-        """,
+            """,
             {"user_id": user_id, "profile_picture": profile_picture, "weight": weight, "height": height, "goal_weight": goal_weight },
             fetch=False,
             commit=True,
         )
 
+        run_query(
+            """
+            UPDATE user_immutables 
+            SET  
+                dob = :dob,
+            WHERE user_id = :user_id; 
+            """,
+            {"user_id": user_id, "dob": dob},
+            fetch=False,
+            commit=True,
+        )
     except Exception as e:
         raise e
     
@@ -42,3 +53,87 @@ def onboardCoachSurvey(user_id: int, desc: str, price : float ):
     except Exception as e:
         raise e
 
+
+
+def insertCoachCert(
+    coach_id : int,
+    cert_name :  str,
+    provider_name : str, 
+    description : str,
+    issued_date,
+    expires_date,
+):
+    try: 
+        run_query(
+            """
+            INSERT INTO certifications
+            (
+                coach_id,
+                cert_name,
+                provider_name, 
+                description,
+                issued_date,
+                expires_date,
+            )
+            VALUES
+            (
+                :cid, 
+                :certN,
+                :provN, 
+                :desc, 
+                :id, 
+                :ed
+            )
+            """,
+            {"cid":coach_id, "certN":cert_name, 
+             "provN":provider_name, "desc": description, 
+             "id": issued_date, "ed": expires_date},
+            fetch=False, 
+            commit=True
+        )
+    except Exception as e: 
+        raise e
+    
+def coachAvailability(
+    coach_id,
+    dow,
+    st,
+    et, 
+    rec, 
+    active
+): 
+    try: 
+        run_query(
+            """
+                INSERT INTO coach_availability
+                (
+                coach_id, 
+                day_of_week,
+                start_time,
+                end_time,
+                recurring,
+                active
+                )
+                VALUES 
+                (
+                :cid, 
+                :dow, 
+                :st,
+                :et, 
+                :rec , 
+                :active
+                )
+            """,
+        {    
+            "cid": coach_id,
+            "dow" : dow,
+            "st" : st,
+            "et" : et, 
+            "rec": rec, 
+            "active" : active
+        },
+            fetch=False, 
+            commit=True
+        )
+    except Exception as e: 
+        raise e
