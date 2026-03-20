37152793809cc39a024153f699208780e146b50c
Rishik <rishikreddy.yesgari@gmail.com>
2026-03-20 19:05:52 -0400

    change identity verification for route protection

--- app/routes/auth/me.py
+++ app/routes/auth/me.py
@@ -1,12 +1,15 @@
 from . import auth_bp
 from flask import session
+from app.services.auth.checkUser import checkUserExists
+
 
 @auth_bp.route("/me", methods=["GET"])
 def me():
     if "user_id" not in session:
         return {"authenticated": False}, 401
 
-    return {
-        "authenticated": True,
-        "role": session["role"]
-    }, 200
+    if not checkUserExists(user_id=session["user_id"]):
+        session.clear()  
+        return {"authenticated": False}, 401
+
+    return {"authenticated": True, "role": session["role"]}, 200
--- app/services/auth/checkUser.py
+++ app/services/auth/checkUser.py
@@ -1,15 +1,28 @@
 from app.services import run_query
 
 
-def checkUserExists(email):
+def checkUserExists(user_id=None, email=None):
 
-    query = """
-    SELECT user_id
-    FROM users_immutables
-    WHERE email = :email
-    LIMIT 1
-    """
+    if user_id:
+        query = """
+        SELECT user_id
+        FROM users_immutables
+        WHERE user_id = :value
+        LIMIT 1
+        """
+        params = {"value": user_id}
 
-    result = run_query(query, {"email": email})
+    elif email:
+        query = """
+        SELECT user_id
+        FROM users_immutables
+        WHERE email = :value
+        LIMIT 1
+        """
+        params = {"value": email}
 
+    else:
+        return False
+
+    result = run_query(query, params)
     return bool(result)
