ad33161de54bd4b008bba72c2278974f8cb54243
ksoliman05 <kerloussoliman05@gmail.com>
2026-03-21 11:30:03 -0400

    Fixed issue with AND clause

--- app/services/coach/search_coaches.py
+++ app/services/coach/search_coaches.py
@@ -5,7 +5,7 @@ def search_coaches(name, filters, is_certified=False, max_price=None, min_rating
     #  Base Query..if no filters are applied this will return all coaches. 
     #  We use DISTINCT to avoid getting the same coach multiple times if they several certs.
     
-    query = """
+  query = """
         SELECT 
             u.user_id, u.first_name, u.last_name, c.price, c.coach_description, 
             ROUND(AVG(cr.rating), 1) as avg_rating,
@@ -15,37 +15,32 @@ def search_coaches(name, filters, is_certified=False, max_price=None, min_rating
         JOIN coach c ON u.user_id = c.coach_id
         LEFT JOIN coach_review cr ON c.coach_id = cr.coach_id
         LEFT JOIN certifications cf ON c.coach_id = cf.coach_id
+        WHERE 1=1
     """
-    if is_certified:
-        query += " AND cf.cert_name IS NOT NULL"
-    
-    query += " WHERE 1=1"
     params = {}
 
+    if is_certified:
+        query += " AND cf.cert_name IS NOT NULL"
     if name:
         query += " AND u.first_name LIKE :name"
         params["name"] = f"%{name}%"
-
     if max_price is not None:
         query += " AND c.price <= :max_price"
         params["max_price"] = max_price
-
     if filters:
         for tag in filters:
             query += f" AND c.coach_description LIKE :tag_{tag}"
             params[f"tag_{tag}"] = f"%{tag}%"
-    
-    # Grouping is required when using AVG or COUNT
-    query += " GROUP BY u.user_id, u.first_name, c.price, c.coach_description"
+
+    query += " GROUP BY u.user_id, u.first_name, u.last_name, c.price, c.coach_description"
 
     if min_rating is not None:
         query += " HAVING AVG(cr.rating) >= :min_rating"
         params["min_rating"] = min_rating
 
-    # Sorting (The Ordering)
     if sort_by == "rating":
         query += " ORDER BY avg_rating DESC, review_count DESC"
     else:
-        query += " ORDER BY c.price ASC" # Default to cheapest first
+        query += " ORDER BY c.price ASC"
 
     return run_query(query, params=params, fetch=True, commit=False)
