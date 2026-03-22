e4d0c986f523c90ed0a9db50775c3fe624a9b325
George Attallah <georgeattallah2005@gmail.com>
2026-03-21 23:57:54 -0400

    exception handling

--- app/routes/coach/coachReview.py
+++ app/routes/coach/coachReview.py
@@ -12,20 +12,21 @@ def getCoachReview():
     "coach_last_name": str
     }
     """
-
-    data = request.get_json()
-    u_id = session.get("user_id")
-    role = session.get("role")
-    c_id = data.get("coach_id")
-
-
-    if not u_id or not role:
-        return jsonify({"error": "Unauthorized"}), 401
-    if not c_id: 
-        return jsonify({"no coach provided"}), 500
-
-    return jsonify(getReviews(c_id)), 200
-
+    try : 
+        data = request.get_json()
+        u_id = session.get("user_id")
+        role = session.get("role")
+        c_id = data.get("coach_id")
+    
+    
+        if not u_id or not role:
+            return jsonify({"error": "Unauthorized"}), 401
+        if not c_id: 
+            return jsonify({"error": "no coach provided"}), 500
+    
+        return jsonify(getReviews(c_id)), 200
+    except Exception as e : 
+        raise e 
 @coach_bp.route("/leave_review", methods=["POST"])
 def leaveCoachReview():
     """
@@ -34,27 +35,26 @@ def leaveCoachReview():
     "rating" : int (1-5) must be an int
     "review_text": str
     """
-
-    u_id = session.get("user_id")
-    role = session.get("role")
-    data = request.get_json()
-    c_id = data.get("coach_id")
-
-    if not u_id or not role :
-        return jsonify({"error": "Unauthorized"}), 401
-    if not c_id:
-        return jsonify({"error": "no coach provided"}), 500
-    if  not (role == "client") or not clientKnowsCoach(u_id, c_id) : 
-        return jsonify({"error": "only clients who have worked with a coach can leave a review"}) , 500
+    try: 
+        u_id = session.get("user_id")
+        role = session.get("role")
+        data = request.get_json()
+        c_id = data.get("coach_id")
     
-    rate = data.get("rating")
-    if rate > 5 or rate < 1 or not isinstance(rate, int): 
-        return jsonify({"error": "you must provide an integer value 1 <= rating <= 5"}) , 500
-    review = data.get("review_text")
-    u_id = int(u_id)
-    role = session.get("role")
-    try:
+        if not u_id or not role :
+            return jsonify({"error": "Unauthorized"}), 401
+        if not c_id:
+            return jsonify({"error": "no coach provided"}), 500
+        if  not (role == "client") or not clientKnowsCoach(u_id, c_id) : 
+            return jsonify({"error": "only clients who have worked with a coach can leave a review"}) , 500
+        
+        rate = data.get("rating")
+        if rate > 5 or rate < 1 or not isinstance(rate, int): 
+            return jsonify({"error": "you must provide an integer value 1 <= rating <= 5"}) , 500
+        review = data.get("review_text")
+        u_id = int(u_id)
+        role = session.get("role")
         postReview(u_id, c_id, rate, review)
-    except Exception as e: 
+        return jsonify({"message": "success"}), 200
+    except Exception as e:
         raise e
-    return jsonify({"message": "success"}), 200
