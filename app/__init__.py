807291cd245def9a70061e9ad78030632058b35c
Rishik <rishikreddy.yesgari@gmail.com>
2026-03-26 18:14:46 -0400

    Added socket functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+socketio = SocketIO(cors_allowed_origins="http://localhost:5173") 
+
+online_users = {}
+socket_to_user = {}
+current_chat = {} 
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from app.config import Config
 from . import db, bcrypt
+from . import socketio
 
 
 def create_app():
@@ -11,13 +12,18 @@ def create_app():
 
     db.init_app(app)
     bcrypt.init_app(app)
+    socketio.init_app(app)
 
     with app.app_context():
         from app.routes import register_routes
 
         register_routes(app)
 
+        from app.sockets import register_socket_events
+
+        register_socket_events(socketio)
+
     return app
 
-app = create_app()
 
+app = create_app()
