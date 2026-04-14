230bfea80e2fcd3487ddf9a887ac7a0e6ebcd0bf
grugg1233 <gma35@njit.edu>
2026-04-13 23:11:15 -0400

    adding missing routes to init

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,6 +7,7 @@ from .nutrition import nutrition_bp
 from .exercises import exercise_bp
 from .contracts import contract_bp
 from .workouts import workoutAction_bp, exerciseLog_bp, workouts_bp
+from .landing import landing_bp
 
 def register_routes(app):
     app.register_blueprint(test_bp, url_prefix="/test")
@@ -18,3 +19,6 @@ def register_routes(app):
     app.register_blueprint(workoutAction_bp, url_prefix="/workoutAction")    
     app.register_blueprint(exerciseLog_bp, url_prefix="/exerciseLog")
     app.register_blueprint(workouts_bp, url_prefix="/workouts")    
+    app.register_blueprint(nutrition_bp, url_prefix="/nutrition")
+    app.register_blueprint(exercise_bp, url_prefix="/exercise")
+    app.register_blueprint(contract_bp, url_prefix="/contract")
