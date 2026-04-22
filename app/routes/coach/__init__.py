b35fc564f28d5d39e39c7b66ec9e718d6e2048e8
ksoliman05 <kerloussoliman05@gmail.com>
2026-04-21 22:47:39 -0400

    Implemented routes allowing coaches to add new as well as update and delete existing certifications from their profile.

--- app/routes/coach/__init__.py
+++ app/routes/coach/__init__.py
@@ -6,8 +6,10 @@ coach_bp = Blueprint("coach", __name__)
 from . import searchCoaches
 from . import updateCoach
 from . import updateCerts
+from . import addCerts
 from . import updateAvailability
 from . import coachReview
 from . import getClients
 from . import assignClientPlan 
 from . import getCoachProfile
+from . import deleteCerts
--- app/routes/coach/updateCerts.py
+++ app/routes/coach/updateCerts.py
@@ -1,17 +1,45 @@
-from flask import Blueprint, request, jsonify, session
-from app.services.coach.update_certs import add_coach_certification
+from flask import request, jsonify, session
 from . import coach_bp
+from app.services.coach.update_Certs import update_coach_certification
 
-@coach_bp.route("/certificates", methods=["POST"])
-def add_cert():
-
-    coach_id = session.get("user_id")
-
-    data = request.get_json()
+@coach_bp.route("/certifications/update", methods=["PATCH"])
+def update_certification_route():
     try:
-        result = add_coach_certification(coach_id, data)
-        return jsonify(result), 201
-    
+
+        coach_id = session.get("user_id")
+        if not coach_id:
+            return jsonify({"error": "Unauthorized"}), 401
+
+        data = request.get_json() or {}
+        
+
+        role = data.get("role")
+        cert_id = data.get("cert_id")
+
+        if role != "coach":
+            return jsonify({"error": "Forbidden. Coach role required."}), 403
+        
+        if not cert_id:
+            return jsonify({"error": "cert_id is required in the request body"}), 400
+
+        cert_name = data.get("cert_name")
+        provider = data.get("provider_name")
+        description = data.get("description")
+        expires_date = data.get("expires_date")
+
+        if not cert_name or not provider:
+            return jsonify({"error": "Certificate name and provider are required"}), 400
+
+        update_coach_certification(
+            coach_id=int(coach_id),
+            cert_id=int(cert_id),
+            cert_name=cert_name,
+            provider=provider,
+            description=description,
+            expires_date=expires_date
+        )
+
+        return jsonify({"message": "Certification updated successfully"}), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
